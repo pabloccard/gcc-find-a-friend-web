ef196d03607b5b68727876db74ea2686d4096e06
pabloccard <pabloccard@gmail.com>
2023-03-30 21:06:35 -0300

    feat(Home): Add all elements and styling

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
-import { Container } from './styles'
+import * as S from './styles'
+import logo from '@/assets/icons/logo-company.svg'
+import dogs from '@/assets/images/dogs.svg'
+import search from '@/assets/icons/search.svg'
 
 export function Home() {
   function handleSearchPets() {
@@ -13,5 +16,45 @@ export function Home() {
     // TO DO
   }
 
-  return <Container>{/* COMECE POR AQUI */}</Container>
+  return (
+    <S.Container>
+      <S.Content>
+        <S.Header>
+          <img src={logo} alt="Find a friend" />
+        </S.Header>
+
+        <S.Hero>
+          <h1>
+            Leve <br /> a felicidade <br /> para o seu lar
+          </h1>
+          <img src={dogs} alt="Cachorros com semblantes contentes" />
+        </S.Hero>
+
+        <S.Footer>
+          <p>
+            Encontre o animal de estimação ideal <br /> para seu estilo de vida!
+          </p>
+
+          <S.SearchForm>
+            <span>Busque um amigo:</span>
+            <S.CitySelect name="" id="">
+              <option value="">PE</option>
+            </S.CitySelect>
+
+            <S.StateSelect htmlFor="state">
+              <select name="" id="state">
+                <option value="1">Recife</option>
+                <option value="2">Goiatuba</option>
+                <option value="3">Morrinhos</option>
+              </select>
+            </S.StateSelect>
+
+            <button>
+              <img src={search} alt="" />
+            </button>
+          </S.SearchForm>
+        </S.Footer>
+      </S.Content>
+    </S.Container>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,109 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  background-color: #f15156;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: calc(1215px + 3rem);
+  padding-top: 0 1.5rem;
+`
+
+export const Header = styled.header`
+  margin-bottom: 0.75rem;
+`
+
+export const Hero = styled.main`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 4.5rem;
+    line-height: 90%;
+    letter-spacing: -0.02em;
+  }
+`
+
+export const Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 7.875rem;
+
+  p {
+    font-size: 1.5rem;
+    font-weight: 600;
+    line-height: 142%;
+  }
+`
+
+export const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+
+  span {
+    white-space: nowrap;
+    margin-right: 1.5rem;
+  }
+
+  button {
+    background: #f4d35e;
+    border: 0;
+    border-radius: 20px;
+    line-height: 0;
+
+    margin-left: 2rem;
+
+    width: 4.5rem;
+    height: 4.5rem;
+  }
+`
+
+export const CitySelect = styled.select`
+  width: 4.5rem;
+  height: 4.5rem;
+
+  background: transparent;
+  border: 1px solid #fff;
+  border-radius: 20px;
+  padding-right: 0.5rem;
+  margin-right: 0.625rem;
+
+  color: #fff;
+  font-size: 1.25rem;
+  line-height: 34px;
+  font-weight: 700;
+  text-align: center;
+`
+
+export const StateSelect = styled.label`
+  display: flex;
+  justify-content: center;
+
+  width: 17.65rem;
+  height: 4.5rem;
+
+  background: #e44449;
+  border-radius: 20px;
+  cursor: pointer;
+
+  select {
+    background: transparent;
+    border: 0;
+
+    color: #fff;
+    font-size: 1.25rem;
+    line-height: 34px;
+    font-weight: 800;
+    text-align: center;
+  }
+`
